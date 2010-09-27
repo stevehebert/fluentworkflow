3df19dbf35c0aa599a695fe2ab2735915179df55
sdhebert <none@none>
2010-09-27 01:55:14 -0500

    added state step configuration tests

--- src/metaworkflow.core.unittest/Builder/StateStepConfigurationTests.cs
+++ src/metaworkflow.core.unittest/Builder/StateStepConfigurationTests.cs
@@ -21,6 +21,28 @@ namespace metaworkflow.core.unittest.Builder
             Assert.That(config.PermittedTriggers.Where(p => p.Key == TriggerType.Submit).First().Value, Is.EqualTo(StateType.New));
         }
 
-        
+        [Test]
+        public void verify_state_entrance_and_exit_declarations()
+        {
+            var config = new StateStepConfiguration<StateType, TriggerType, TriggerContext>(StateType.Rejected);
+
+            config.OnEntry<Step1>(StepPriority.Medium);
+            config.OnExit<Step2>(StepPriority.Highest);
+            
+
+            Assert.That(config.State, Is.EqualTo(StateType.Rejected));
+            Assert.That(
+                config.StateStepInfos.Where(
+                    p =>
+                    p.ActionType == WorkflowStepActionType.Entry && p.Priority == (int) StepPriority.Medium &&
+                    p.StateStepType == typeof (Step1)).Count(), Is.EqualTo(1));
+
+
+            Assert.That(
+                config.StateStepInfos.Where(
+                    p =>
+                    p.ActionType == WorkflowStepActionType.Exit && p.Priority == (int)StepPriority.Highest &&
+                    p.StateStepType == typeof(Step2)).Count(), Is.EqualTo(1));
+        }
     }
 }
---
+++ src/metaworkflow.core.unittest/Step1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using metaworkflow.core.unittest.enums;
+
+namespace metaworkflow.core.unittest
+{
+    public class Step1 : IStateStep<StateType, TriggerType, TriggerContext>
+    {
+        public void Execute(StateStepInfo<StateType, TriggerType, TriggerContext> stateStepInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
