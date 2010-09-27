f3e0a474ada1710440dddb5e0c7783191bab0fb2
sdhebert <none@none>
2010-09-27 02:14:27 -0500

    Added the workflow module

--- src/metaworkflow/WorkflowModule.cs
+++ src/metaworkflow/WorkflowModule.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Autofac;
+using metaworkflow.core.Builder;
 
 namespace metaworkflow.core
 {
-    class WorkflowModule
+    public abstract class WorkflowModule<TWorkflow, TState, TTrigger, TTriggerContext> : Module
     {
+        public abstract void Configure(IWorkflowBuilder<TWorkflow, TState, TTrigger, TTriggerContext> builder);
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            var workflowBuilder = new WorkflowBuilder<TWorkflow, TState, TTrigger, TTriggerContext>();
+
+            Configure(workflowBuilder);
+        }
     }
 }
