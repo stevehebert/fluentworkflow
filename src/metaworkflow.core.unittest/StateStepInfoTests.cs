73d215c5f8bc591e650ad0833803d9cdfb237e43
sdhebert <none@none>
2010-09-27 01:09:45 -0500

    added state step info tests

---
+++ src/metaworkflow.core.unittest/StateStepInfoTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using metaworkflow.core.Builder;
+using metaworkflow.core.unittest.enums;
+using NUnit.Framework;
+using Stateless;
+
+namespace metaworkflow.core.unittest
+{
+    [TestFixture]
+    public class StateStepInfoTests
+    {
+        [Test]
+        public void set_trigger_execution_with_existing_context()
+        {
+            var triggerTrip = new TriggerTrip
+                <TriggerType,
+                    TriggerContext>();
+
+            var transition = new StateMachine<StateType, TriggerType>.Transition(StateType.New, StateType.UnderReview,
+                                                                     TriggerType.Ignore);
+
+            var stateStepInfo = new StateStepInfo<StateType, TriggerType, TriggerContext>(new TriggerContext {DocumentId = 5}, transition, triggerTrip);
+
+            stateStepInfo.Fire(TriggerType.Submit);
+
+            Assert.That(triggerTrip.Trigger, Is.EqualTo(TriggerType.Submit));
+            Assert.That(triggerTrip.TriggerContext.DocumentId, Is.EqualTo(5));
+        }
+
+
+        [Test]
+        public void set_trigger_execution_with_new_context()
+        {
+            var triggerTrip = new TriggerTrip<TriggerType,TriggerContext>();
+
+            var transition = new StateMachine<StateType, TriggerType>.Transition(StateType.New, StateType.UnderReview,
+                                                                                 TriggerType.Ignore);
+
+            var stateStepInfo = new StateStepInfo<StateType, TriggerType, TriggerContext>(new TriggerContext { DocumentId = 5 }, transition, triggerTrip);
+
+            stateStepInfo.Fire(TriggerType.Submit, new TriggerContext{DocumentId=6});
+
+            Assert.That(triggerTrip.Trigger, Is.EqualTo(TriggerType.Submit));
+            Assert.That(triggerTrip.TriggerContext.DocumentId, Is.EqualTo(6));
+        }
+
+    }
+}
--- src/metaworkflow/StateStepInfo.cs
+++ src/metaworkflow/StateStepInfo.cs
@@ -1,7 +1,15 @@
-﻿using Stateless;
+﻿using System;
+using metaworkflow.core.Builder;
+using Stateless;
 
 namespace metaworkflow.core
 {
+    /// <summary>
+    /// Contextual information for the current state transition process
+    /// </summary>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    /// <typeparam name="TTrigger">The type of the trigger.</typeparam>
+    /// <typeparam name="TTriggerContext">The type of the trigger context.</typeparam>
     public class StateStepInfo<TState, TTrigger, TTriggerContext>
     {
         /// <summary>
@@ -10,18 +18,47 @@ namespace metaworkflow.core
         /// <value>The workflow context.</value>
         public TTriggerContext Context { get; private set; }
 
+        /// <summary>
+        /// Fires the specified trigger along with context information
+        /// </summary>
+        /// <param name="trigger">The trigger.</param>
+        /// <param name="triggerContext">The trigger context.</param>
+        /// <exception cref="InvalidOperationException">thrown when a trigger has previously been set</exception>
         public void Fire(TTrigger trigger, TTriggerContext triggerContext)
         {
+            _triggerTrip.SetTrigger(trigger, triggerContext);
         }
 
+        /// <summary>
+        /// Fires the specified trigger using the existing context
+        /// </summary>
+        /// <param name="trigger">The trigger.</param>
+        /// <exception cref="InvalidOperationException">thrown when a trigger has previously been set</exception>
         public void Fire(TTrigger trigger)
-        { }
+        {
+            _triggerTrip.SetTrigger(trigger, Context);
+        }
 
+        /// <summary>
+        /// Gets the state transition information
+        /// </summary>
+        /// <value>The transition info.</value>
         public TransitionInfo<TState, TTrigger> TransitionInfo { get; private set; }
 
-        public StateStepInfo(TTriggerContext triggerContext, StateMachine<TState, TTrigger>.Transition transition)
+        private readonly TriggerTrip<TTrigger, TTriggerContext> _triggerTrip;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StateStepInfo&lt;TState, TTrigger, TTriggerContext&gt;"/> class.
+        /// </summary>
+        /// <param name="triggerContext">The trigger context.</param>
+        /// <param name="transition">The transition information from the state machine.</param>
+        /// <param name="triggerTrip">The trigger trip.</param>
+        public StateStepInfo(TTriggerContext triggerContext, 
+                             StateMachine<TState, TTrigger>.Transition transition, 
+                             TriggerTrip<TTrigger, TTriggerContext> triggerTrip)
         {
             Context = triggerContext;
+            _triggerTrip = triggerTrip;
 
             TransitionInfo = new TransitionInfo<TState, TTrigger>(transition);
         }
