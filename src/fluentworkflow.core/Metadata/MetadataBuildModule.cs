0571e2e9db1cae821798bd5acf4acaa9703cde99
sdhebert <none@none>
2010-12-14 05:32:19 -0600

    added changes to metadata build module to use a concrete type for metadata
    --HG--
    branch : sampleapp

--- src/fluentworkflow.core/Metadata/MetadataBuildModule.cs
+++ src/fluentworkflow.core/Metadata/MetadataBuildModule.cs
@@ -31,28 +31,54 @@ namespace fluentworkflow.core.Metadata
         }
     }
 
-
-    public class MetadataBuildModule<TType, TWorkflow, TState, TDetail> : MetadataModule<TType, IEnumerable<MetadataInstance<TWorkflow, TState, TDetail>>>
+    public class TypeMapperRoot<TType, TWorkflow, TState, TDetail>
     {
+        private readonly IList<MetadataInstance<TWorkflow, TState, TDetail>> _metadataInstances;
 
+        public TypeMapperRoot(IList<MetadataInstance<TWorkflow, TState, TDetail>> metadataInstances)
+        {
+            _metadataInstances = metadataInstances;
+        }
 
-        private readonly IList<MetadataInstance<TWorkflow, TState, TDetail>> _metadataInstances =
-            new List<MetadataInstance<TWorkflow, TState, TDetail>>();
 
         public ITypeMapper<TType, TWorkflow, TState, TDetail> For(TWorkflow workflow, TState state)
         {
             return new TypeMapper<TType, TWorkflow, TState, TDetail>(workflow, state, _metadataInstances);
         }
         
-        public override void Register(IMetadataRegistrar<TType, IEnumerable<MetadataInstance<TWorkflow, TState, TDetail>>> registrar)
+    }
+
+    public interface IMetadataInfo<TWorkflow, TState, TDetail>
+    {
+        IEnumerable<MetadataInstance<TWorkflow, TState, TDetail>> MetadataInstances { get; set; }
+    }
+
+    public class MetadataInfo<TWorkflow, TState, TDetail> :IMetadataInfo<TWorkflow,TState, TDetail>
+    {
+        public IEnumerable<MetadataInstance<TWorkflow, TState, TDetail>> MetadataInstances { get; set; }
+    }
+
+    public abstract class  MetadataBuildModule<TType, TWorkflow, TState, TDetail> : MetadataModule<TType, IMetadataInfo<TWorkflow, TState, TDetail>>
+    {
+
+        private readonly IList<MetadataInstance<TWorkflow, TState, TDetail>> _metadataInstances =
+            new List<MetadataInstance<TWorkflow, TState, TDetail>>();
+
+        public abstract void BuildMapping(TypeMapperRoot<TType, TWorkflow, TState, TDetail> mapperRoot);
+       
+        public override void Register(IMetadataRegistrar<TType, IMetadataInfo<TWorkflow, TState, TDetail>> registrar)
         {
+            BuildMapping(new TypeMapperRoot<TType, TWorkflow, TState, TDetail>(_metadataInstances));
+
             var items = from p in _metadataInstances
                         group p by p.Type
                         into q
                         select q;
 
             foreach (var item in items)
-                registrar.RegisterType(item.Key, item);
+                registrar.RegisterType(item.Key,
+                                       new MetadataInfo<TWorkflow, TState, TDetail>
+                                           {MetadataInstances = from t in item select t});
 
             registrar.ContainerBuilder.RegisterType<MetadataResolver<TType, TWorkflow, TState, TDetail>>().As<IMetadataResolver<TType, TWorkflow, TState, TDetail>>();
         }
