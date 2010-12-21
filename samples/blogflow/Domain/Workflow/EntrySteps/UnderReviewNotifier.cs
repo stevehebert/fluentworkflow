3bb3704238630379e866a6b9dd21f4b54810a48f
s_hebert <none@none>
2010-12-21 00:09:22 -0600

    added comments to the under review notifier
    --HG--
    branch : sampleapp-refactor

--- samples/blogflow/Domain/Workflow/EntrySteps/UnderReviewNotifier.cs
+++ samples/blogflow/Domain/Workflow/EntrySteps/UnderReviewNotifier.cs
@@ -4,14 +4,26 @@ using fluentworkflow.core;
 
 namespace blogflow.Domain.Workflow.EntrySteps
 {
+    /// <summary>
+    /// Responsible for initiating notifications based on a document entering the under review state
+    /// </summary>
     public class UnderReviewNotifier : IEntryStateStep<WorkflowState, StateTrigger, IDocumentContext>
     {
         private readonly IContextNotification _contextNotification;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnderReviewNotifier"/> class.
+        /// </summary>
+        /// <param name="contextNotification">The context notification.</param>
         public UnderReviewNotifier(IContextNotification contextNotification)
         {
             _contextNotification = contextNotification;
         }
 
+        /// <summary>
+        /// Executes the specified state step notifying when a document is under review
+        /// </summary>
+        /// <param name="stateStepInfo">The state step info.</param>
         public void Execute(StateStepInfo<WorkflowState, StateTrigger, IDocumentContext> stateStepInfo)
         {
             _contextNotification.NotifyUnderReviewStatus(stateStepInfo.Context);
