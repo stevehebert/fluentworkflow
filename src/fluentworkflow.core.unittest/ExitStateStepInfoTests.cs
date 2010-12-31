c609f4be7c61d7299970d8092c6fa563b8ead9bb
s_hebert <none@none>
2010-12-31 06:03:27 -0600

    added exit state step info and associated unit tests
    --HG--
    branch : sampleapp-refactor

---
+++ src/fluentworkflow.core.unittest/ExitStateStepInfoTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using fluentworkflow.core.unittest.enums;
+using NUnit.Framework;
+using Stateless;
+
+namespace fluentworkflow.core.unittest
+{
+    [TestFixture]
+    public class ExitStateStepInfoTests
+    {
+        [Test]
+        public void verify_context_passthrough()
+        {
+            var transition = new StateMachine<StateType, TriggerType>.Transition(StateType.New, StateType.UnderReview,
+                                                                                 TriggerType.Ignore);
+
+            var stateStepInfo = new ExitStateStepInfo<StateType, TriggerType, TriggerContext>(new TriggerContext { DocumentId = 5 }, transition);
+
+            Assert.That(stateStepInfo.Context.DocumentId, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void verify_transition_passthrough()
+        {
+            var transition = new StateMachine<StateType, TriggerType>.Transition(StateType.New, StateType.UnderReview,
+                                                                                 TriggerType.Ignore);
+
+            var stateStepInfo = new ExitStateStepInfo<StateType, TriggerType, TriggerContext>(new TriggerContext { DocumentId = 5 }, transition);
+
+            Assert.That(stateStepInfo.StateExitTransitionInfo.CurrentState, Is.EqualTo(StateType.New));
+            Assert.That(stateStepInfo.StateExitTransitionInfo.NextState, Is.EqualTo(StateType.UnderReview));
+        }
+
+    }
+}
---
+++ src/fluentworkflow.core/ExitStateStepInfo.cs
@@ -0,0 +1,33 @@
+﻿using Stateless;
+
+namespace fluentworkflow.core
+{
+    public class ExitStateStepInfo<TState, TTrigger, TTriggerContext>
+    {
+        /// <summary>
+        /// Gets the workflow context
+        /// </summary>
+        /// <value>The workflow context.</value>
+        public TTriggerContext Context { get; private set; }
+
+        /// <summary>
+        /// Gets the state transition information
+        /// </summary>
+        /// <value>The transition info.</value>
+        public StateExitTransitionInfo<TState, TTrigger> StateExitTransitionInfo { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExitStateStepInfo{TState,TTrigger,TTriggerContext}"/> class.
+        /// </summary>
+        /// <param name="triggerContext">The trigger context.</param>
+        /// <param name="transition">The transition information from the state machine.</param>
+        public ExitStateStepInfo(TTriggerContext triggerContext, 
+                             StateMachine<TState, TTrigger>.Transition transition)
+                             
+        {
+            Context = triggerContext;
+
+            StateExitTransitionInfo = new StateExitTransitionInfo<TState, TTrigger>(transition);
+        }
+    }
+}
