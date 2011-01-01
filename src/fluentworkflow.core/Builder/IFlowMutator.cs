e0d82e4648fa8dd6bb7d46dcdd8c60f9cacb96e9
s_hebert <none@none>
2011-01-01 10:07:41 -0600

    removed the trigger context from iflowmutator as this is duplication of data provided to the state step

--- src/fluentworkflow.core/Builder/IFlowMutator.cs
+++ src/fluentworkflow.core/Builder/IFlowMutator.cs
@@ -9,13 +9,6 @@
         TTrigger Trigger { get; }
 
         /// <summary>
-        /// Gets the declared context.
-        /// </summary>
-        /// <value>The trigger context.</value>
-        TTriggerContext TriggerContext { get; }
-
-
-        /// <summary>
         /// Gets the indicator of whether a trigger has been set
         /// </summary>
         /// <value><c>true</c> if this instance is set; otherwise, <c>false</c>.</value>
@@ -27,14 +20,11 @@
         /// <param name="trigger">The trigger.</param>
         /// <param name="triggerContext">The trigger context.</param>
         void SetTrigger(TTrigger trigger, TTriggerContext triggerContext);
-
-
+        
         /// <summary>
         /// Sets the trigger implying that the context remains the same.
         /// </summary>
         /// <param name="trigger">The trigger.</param>
         void SetTrigger(TTrigger trigger);
     }
-
-
 }
