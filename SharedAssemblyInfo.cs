81da767bda1a72a2286ec71683428145470da611
s_hebert <none@none>
2011-01-19 14:29:08 -0600

    removed the hard reference to SharedAssemblyInfo as it's now generated by the build

--- SharedAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("Hebert Software Services")]
-[assembly: AssemblyProduct("fluentworkflow")]
-[assembly: AssemblyCopyright("Copyright Hebertsoft 2010-2011")]
-[assembly: AssemblyVersion("0.0.8.102")]
-[assembly: AssemblyFileVersion("0.0.8.102")]
-
