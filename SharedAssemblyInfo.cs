f9a3c52f7a353f2b3b7e61eb297c4f41dc112ee5
sdhebert <none@none>
2011-02-15 15:39:09 -0600

    made the project first-pull friendly

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Hebert Software Services")]
+[assembly: AssemblyProduct("fluentworkflow")]
+[assembly: AssemblyCopyright("Copyright Hebertsoft 2010-2011")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
+
